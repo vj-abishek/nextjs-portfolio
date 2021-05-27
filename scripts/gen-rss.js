1b5c34873b3f19e82944cc4df4c9dfb528517a2f
Abishek PY <43115551+vj-abishek@users.noreply.github.com>
2021-05-27 13:26:18 +0530

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Abishek',
+    site_url: 'https://abi.vercel.app',
+    feed_url: 'https://abi.vercel.app/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
